li-qixin/tarox

🐳 chore: first commit

Makefile
@@ -1,0 +1,104 @@
+ifeq ($(wildcard .git),)
+	$(error You need to download this project by git! Aborting!)
+endif
+
+
+all: flysq_v1_default
+
+space := $(subst ,, )
+
+
+# Parsing
+# -------------------------------------------- 
+FIRST_ARG := $(firstword $(MAKECMDGOALS))
+ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+
+MAKE_PID := $(shell echo $$PPID)
+
+
+j := $(shell ps T | sed -n "s|.*$(MAKE_PID).*$(MAKE) *\(-j\) *\([0-9][0-9]*\).*|\2|p")
+j_clang_tidy := $(or $(j),4)
+
+NINJA_BIN := ninja
+ifndef NO_NINJA_BUILD
+	NINJA_BUILD := $(shell $(NINJA_BIN) --version 2>/dev/null)
+	ifndef NINJA_BUILD
+		NINJA_BIN := ninja-build
+		NINJA_BUILD := $(shell $(NINJA_BIN) --version 2>/dev/null)
+	endif
+endif
+
+ifdef NINJA_BUILD
+	TAROX_CMAKE_GENERATOR := Ninja
+	TAROX_MAKE := $(NINJA_BIN)
+
+	ifdef VERBOSE
+		TAROX_MAKE_ARGS := -v
+	else
+		TAROX_MAKE_ARGS :=
+	endif
+
+	ifneq ($(j),)
+		TAROX_MAKE_ARGS := $(TAROX_MAKE_ARGS) -j$(j)
+	endif
+else
+	ifdef SYSTEMROOT
+		TAROX_CMAKE_GENERATOR := "MSYS\ Makefiles"
+	else
+		TAROX_CMAKE_GENERATOR := "Unix\ Makefiles"
+	endif
+
+	j := $(or $(j),4)
+	TAROX_MAKE = $(MAKE)
+	TAROX_MAKE_ARGS := -j$(j) --no-print-directory
+endif
+
+SRC_DIR := $(shell dirname "$(realpath $(lastword $(MAKEFILE_LIST)))")
+
+CMAKE_ARGS ?=
+
+ifdef EXTERNAL_MODULES_LOCATION
+	override CMAKE_ARGS += -DEXTERNAL_MODULES_LOCATION:STRING=$(EXTERNAL_MODULES_LOCATION)
+endif
+
+ifdef TAROX_CMAKE_BUILD_TYPE
+	override CMAKE_ARGS += -DCMAKE_BUILD_TYPE=$(TAROX_CMAKE_BUILD_TYPE)
+endif
+
+define cmake-build
+	$(eval override CMAKE_ARGS += -DTAROX_CONFIG=$(1))
+	@$(eval BUILD_DIR = "$(SRC_DIR)/build/$(1)")
+	@$(call cmake-cache-check)
+	@if [ $(TAROX_CMAKE_GENERATOR) = "Ninja" ] && [ -e $(BUILD_DIR)/Makefile ]; then rm -rf $(BUILD_DIR); fi
+	@if [ $(TAROX_CMAKE_GENERATOR) = "Ninja" ] && [ ! -f $(BUILD_DIR)/build.ninja ]; then rm -rf $(BUILD_DIR); fi
+	@if [ ! -e $(BUILD_DIR)/CMakeCache.txt ] || [ $(CMAKE_CACHE_CHECK) ]; then \
+		mkdir -p $(BUILD_DIR) \
+		&& cd $(BUILD_DIR) \
+		&& cmake "$(SRC_DIR)" -G"$(TAROX_CMAKE_GENERATOR)" $(CMAKE_ARGS)\
+		|| (rm -rf $(BUILD_DIR)); \
+	fi
+	@cmake --build $(BUILD_DIR) -- $(TAROX_MAKE_ARGS) $(ARGS)
+endef
+
+define cmake-cache-check
+	@$(eval CACHED_CMAKE_OPTIONS = $(shell cd $(BUILD_DIR) 2>/dev/null && cmake -L 2>/dev/null | sed -n 's|\([^[:blank:]]*\):[^[:blank:]]*\(=[^[:blank:]]*\)|\1\2|gp'))
+	@$(eval DESIRED_CMAKE_OPTIONS = $(shell echo $(CMAKE_ARGS) | sed -n 's|-D\([^[:blank:]]*=[^[:blank:]]*\)|\1|gp'))
+	@$(eval VERIFIED_CMAKE_OPTIONS = $(foreach option,$(DESIRED_CMAKE_OPTIONS),$(strip $(findstring $(option)$(space),$(CACHED_CMAKE_OPTIONS)))))
+	@$(eval CMAKE_CACHE_CHECK = $(if $(findstring $(DESIRED_CMAKE_OPTIONS),$(VERIFIED_CMAKE_OPTIONS)),,y))
+endef
+
+ALL_CONFIG_TARGETS := $(shell find boards -maxdepth 3 -mindepth 3 -name '*.taroxconfig' -print | sed -e 's|boards\/||' | sed -e 's|\.taroxconfig||' | sed -e 's|\/|_|g' | sort)
+
+CONFIG_TARGETS_DEFAULT := $(patsubst %_default,%,$(filter %_default,$(ALL_CONFIG_TARGETS)))
+$(ALL_CONFIG_TARGETS):
+	@$(call cmake-build,$@)
+
+$(CONFIG_TARGETS_DEFAULT):
+	@$(call cmake-build,$@_default)
+
+%:
+	$(if $(filter $(FIRST_ARG),$@),\
+		$(error "Make target $@ not found. It either does not exist or $@ can't be the first argument. Use 'make list_config_targets' to get a list of all possible [configuration] targets."),@#)
+
+list_config_targets:
+	@for targ in $(patsubst %_default,%[_default],$(ALL_CONFIG_TARGETS)); do echo $$targ; done

li-qixin/FlySQ

Makefile
@@ -1,0 +1,105 @@
+ifeq ($(wildcard .git),)
+	$(error You need to download this project by git! Aborting!)
+endif
+
+
+all: flysq_v1_default
+
+space := $(subst ,, )
+
+
+# Parsing
+# -------------------------------------------- 
+FIRST_ARG := $(firstword $(MAKECMDGOALS))
+ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+
+MAKE_PID := $(shell echo $$PPID)
+
+
+j := $(shell ps T | sed -n "s|.*$(MAKE_PID).*$(MAKE) *\(-j\) *\([0-9][0-9]*\).*|\2|p")
+j_clang_tidy := $(or $(j),4)
+
+NINJA_BIN := ninja
+ifndef NO_NINJA_BUILD
+	NINJA_BUILD := $(shell $(NINJA_BIN) --version 2>/dev/null)
+	ifndef NINJA_BUILD
+		NINJA_BIN := ninja-build
+		NINJA_BUILD := $(shell $(NINJA_BIN) --version 2>/dev/null)
+	endif
+endif
+
+ifdef NINJA_BUILD
+	TAROX_CMAKE_GENERATOR := Ninja
+	TAROX_MAKE := $(NINJA_BIN)
+
+	ifdef VERBOSE
+		TAROX_MAKE_ARGS := -v
+	else
+		TAROX_MAKE_ARGS :=
+	endif
+
+	ifneq ($(j),)
+		TAROX_MAKE_ARGS := $(TAROX_MAKE_ARGS) -j$(j)
+	endif
+else
+	ifdef SYSTEMROOT
+		TAROX_CMAKE_GENERATOR := "MSYS\ Makefiles"
+	else
+		TAROX_CMAKE_GENERATOR := "Unix\ Makefiles"
+	endif
+
+	j := $(or $(j),4)
+	TAROX_MAKE = $(MAKE)
+	TAROX_MAKE_ARGS := -j$(j) --no-print-directory
+endif
+
+SRC_DIR := $(shell dirname "$(realpath $(lastword $(MAKEFILE_LIST)))")
+
+CMAKE_ARGS ?=
+
+ifdef EXTERNAL_MODULES_LOCATION
+	override CMAKE_ARGS += -DEXTERNAL_MODULES_LOCATION:STRING=$(EXTERNAL_MODULES_LOCATION)
+endif
+
+ifdef TAROX_CMAKE_BUILD_TYPE
+	override CMAKE_ARGS += -DCMAKE_BUILD_TYPE=$(TAROX_CMAKE_BUILD_TYPE)
+endif
+
+define cmake-build
+	$(eval override CMAKE_ARGS += -DTAROX_CONFIG_NAME=$(1))
+	@$(eval BUILD_DIR = "$(SRC_DIR)/build/$(1)")
+	@$(call cmake-cache-check)
+	@if [ $(TAROX_CMAKE_GENERATOR) = "Ninja" ] && [ -e $(BUILD_DIR)/Makefile ]; then rm -rf $(BUILD_DIR); fi
+	@if [ $(TAROX_CMAKE_GENERATOR) = "Ninja" ] && [ ! -f $(BUILD_DIR)/build.ninja ]; then rm -rf $(BUILD_DIR); fi
+	@if [ ! -e $(BUILD_DIR)/CMakeCache.txt ] || [ $(CMAKE_CACHE_CHECK) ]; then \
+		mkdir -p $(BUILD_DIR) \
+		&& cd $(BUILD_DIR) \
+		&& cmake "$(SRC_DIR)" -G"$(TAROX_CMAKE_GENERATOR)" \
+		-D CMAKE_EXPORT_COMPILE_COMMANDS=ON $(CMAKE_ARGS)\
+		|| (rm -rf $(BUILD_DIR)); \
+	fi
+	@cmake --build $(BUILD_DIR) -- $(TAROX_MAKE_ARGS) $(ARGS)
+endef
+
+define cmake-cache-check
+	@$(eval CACHED_CMAKE_OPTIONS = $(shell cd $(BUILD_DIR) 2>/dev/null && cmake -L 2>/dev/null | sed -n 's|\([^[:blank:]]*\):[^[:blank:]]*\(=[^[:blank:]]*\)|\1\2|gp'))
+	@$(eval DESIRED_CMAKE_OPTIONS = $(shell echo $(CMAKE_ARGS) | sed -n 's|-D\([^[:blank:]]*=[^[:blank:]]*\)|\1|gp'))
+	@$(eval VERIFIED_CMAKE_OPTIONS = $(foreach option,$(DESIRED_CMAKE_OPTIONS),$(strip $(findstring $(option)$(space),$(CACHED_CMAKE_OPTIONS)))))
+	@$(eval CMAKE_CACHE_CHECK = $(if $(findstring $(DESIRED_CMAKE_OPTIONS),$(VERIFIED_CMAKE_OPTIONS)),,y))
+endef
+
+ALL_CONFIG_TARGETS := $(shell find boards -maxdepth 3 -mindepth 3 -name '*.taroxconfig' -print | sed -e 's|boards\/||' | sed -e 's|\.taroxconfig||' | sed -e 's|\/|_|g' | sort)
+
+CONFIG_TARGETS_DEFAULT := $(patsubst %_default,%,$(filter %_default,$(ALL_CONFIG_TARGETS)))
+$(ALL_CONFIG_TARGETS):
+	@$(call cmake-build,$@)
+
+$(CONFIG_TARGETS_DEFAULT):
+	@$(call cmake-build,$@_default)
+
+%:
+	$(if $(filter $(FIRST_ARG),$@),\
+		$(error "Make target $@ not found. It either does not exist or $@ can't be the first argument. Use 'make list_config_targets' to get a list of all possible [configuration] targets."),@#)
+
+list_config_targets:
+	@for targ in $(patsubst %_default,%[_default],$(ALL_CONFIG_TARGETS)); do echo $$targ; done